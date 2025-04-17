c9def42899b1952078f3eb2dcd1120d6452fcf8a
Lucas Gromowski <lcs.gr.pzn@gmail.com>
2025-04-17 14:17:40 -0300

    Resolução do 8º exercício

--- little-sisters-vocab/strings.py
+++ little-sisters-vocab/strings.py
@@ -1,17 +1,18 @@
 """Functions for creating, transforming, and adding prefixes to strings."""
 
 
-def add_prefix_un(word):
+def add_prefix_un(word: str):
     """Take the given word and add the 'un' prefix.
 
     :param word: str - containing the root word.
     :return: str - of root word prepended with 'un'.
     """
 
-    pass
+    new_word = "un" + word
+    return new_word
 
 
-def make_word_groups(vocab_words):
+def make_word_groups(vocab_words: list) -> str:
     """Transform a list containing a prefix and words into a string with the prefix followed by the words with prefix prepended.
 
     :param vocab_words: list - of vocabulary words with prefix in first index.
@@ -26,10 +27,19 @@ def make_word_groups(vocab_words):
     produces the following string: 'en :: enclose :: enjoy :: enlighten'.
     """
 
-    pass
+    prefix = vocab_words[0]
+    new_vocab_words = []
+    new_vocab_words.append(prefix)
+
+    for index, word in enumerate(vocab_words):
+        if index == 0:
+            continue
+        new_vocab_words.append(prefix + word)
+
+    return " :: ".join(new_vocab_words)
 
 
-def remove_suffix_ness(word):
+def remove_suffix_ness(word: str): 
     """Remove the suffix from the word while keeping spelling in mind.
 
     :param word: str - of word to remove suffix from.
@@ -38,10 +48,14 @@ def remove_suffix_ness(word):
     For example: "heaviness" becomes "heavy", but "sadness" becomes "sad".
     """
 
-    pass
+    new_word = word[:-4]
+    
+    if new_word.endswith("i"):
+        return new_word.replace("i", "y")
+    return new_word
 
 
-def adjective_to_verb(sentence, index):
+def adjective_to_verb(sentence: str, index: int) -> str:
     """Change the adjective within the sentence to a verb.
 
     :param sentence: str - that uses the word in sentence.
@@ -51,4 +65,8 @@ def adjective_to_verb(sentence, index):
     For example, ("It got dark as the sun set.", 2) becomes "darken".
     """
 
-    pass
+    new_sentence = sentence.replace(".", "")
+    sentence_list = new_sentence.split()
+
+        
+    return sentence_list[index] + "en"
