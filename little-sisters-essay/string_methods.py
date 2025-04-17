450f605f5755a36880260a9e81361d06265eea3e
Lucas Gromowski <lcs.gr.pzn@gmail.com>
2025-04-17 13:13:23 -0300

    Solução do 7º exercício

--- little-sisters-essay/string_methods.py
+++ little-sisters-essay/string_methods.py
@@ -1,38 +1,37 @@
 """Functions to help edit essay homework using string manipulation."""
 
 
-def capitalize_title(title):
+def capitalize_title(title: str):
     """Convert the first letter of each word in the title to uppercase if needed.
 
     :param title: str - title string that needs title casing.
     :return: str - title string in title case (first letters capitalized).
     """
 
-    teste = title
-    return teste
+    return title.title()
 
 
-def check_sentence_ending(sentence):
+def check_sentence_ending(sentence: str) -> bool:
     """Check the ending of the sentence to verify that a period is present.
 
     :param sentence: str - a sentence to check.
     :return: bool - return True if punctuated correctly with period, False otherwise.
     """
 
-    pass
+    return sentence.endswith(".")
 
 
-def clean_up_spacing(sentence):
+def clean_up_spacing(sentence: str):
     """Verify that there isn't any whitespace at the start and end of the sentence.
 
     :param sentence: str - a sentence to clean of leading and trailing space characters.
     :return: str - a sentence that has been cleaned of leading and trailing space characters.
     """
 
-    pass
+    return sentence.strip()
 
 
-def replace_word_choice(sentence, old_word, new_word):
+def replace_word_choice(sentence: str, old_word: str, new_word: str):
     """Replace a word in the provided sentence with a new one.
 
     :param sentence: str - a sentence to replace words in.
@@ -41,4 +40,4 @@ def replace_word_choice(sentence, old_word, new_word):
     :return: str - input sentence with new words in place of old words.
     """
 
-    pass
+    return sentence.replace(old_word, new_word)
