b1b48913aeac3237a8f61a604cc331d50d8d9729
Lucas Gromowski <lcs.gr.pzn@gmail.com>
2025-05-21 16:34:22 -0300

    Solução do 24º exercício

---
+++ bob/bob.py
@@ -0,0 +1,17 @@
+def response(hey_bob: str) -> str:
+    hey_bob = hey_bob.strip()
+
+    if hey_bob.isupper() and hey_bob.endswith('?'):
+        return "Calm down, I know what I'm doing!"
+    
+    if hey_bob.endswith('?'):
+        return "Sure."
+    
+    if hey_bob.isupper():
+        return "Whoa, chill out!"
+    
+    if not hey_bob:
+        return "Fine. Be that way!"
+    
+    else:
+        return "Whatever."
